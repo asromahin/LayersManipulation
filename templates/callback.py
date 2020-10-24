c0de160b5f1e6f89ee048762efc5d81897eb9891
asrom <asromahin@yandex.ru>
2020-10-24 12:48:00 +0700

    add callback

---
+++ templates/callback.py
@@ -0,0 +1,22 @@
+import torch
+
+
+class CallbackLayer(torch.nn.Module):
+    def __init__(
+            self,
+            old_encoder_block,
+            callback=callback_pass,
+    ):
+        super(CallbackLayer, self).__init__()
+        self.old_block = old_encoder_block
+        self.callback = callback
+
+    def forward(self, *args):
+        x = self.old_block(*args)
+        x = self.callback(x)
+        return x
+
+
+def callback_pass(x):
+    return x
+
